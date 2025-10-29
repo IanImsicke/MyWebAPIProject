9104f869115d92c113fcf51e7038bacc6e444ac2
IanImsicke <ianimsicke@gmail.com>
2025-10-29 13:53:12 -0400

    Added NameController.cs

---
+++ MyWebAPIProject/Controllers/NameController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            var data = new { name = "Ian Imsicke" }; 
+            return Ok(data);
+        }
+    }
+}
